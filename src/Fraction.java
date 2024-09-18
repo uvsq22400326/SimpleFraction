3964dd9e97d51ed1e14c09c894784e47d9cc3f50
LOUNICI <lounicighilaspro@gmail.com>
2024-09-18 10:59:54 +0200

    partie 2 q4

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,6 +1,50 @@
 public class Fraction{
-    
-    public String toString(){
-        return "Je suis une fraction.";
+
+    private int numerateur;
+    private int denominateur;
+    private static final Fraction ZERO= new Fraction(0,1);
+    private static final Fraction UN= new Fraction(1,1);
+
+
+    public Fraction(int numerateur,int denominateur){
+        if (denominateur == 0) {
+            throw new IllegalArgumentException("Le dénominateur ne peut pas être zéro.");
+        }
+        this.numerateur=numerateur;
+        this.denominateur=denominateur;
+
     }
+
+    public Fraction(int numerateur){
+        this.numerateur=numerateur;
+        this.denominateur= 1;
+
+    }
+
+    public Fraction(){
+        this.numerateur=0;
+        this.denominateur=1;
+    }
+    public String toString(){
+        return numerateur + "/" + denominateur;
+    }
+
+    public int  getNUMERATEUR(){
+        return this.numerateur;
+    }
+    public int getDENOMINATEUR(){
+        return this.denominateur;
+    }
+    public Fraction getZERO(){
+        return ZERO;
+    }
+    public Fraction getUN(){
+        return UN;
+    }
+
+
+
 }
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //test de Fration sans argument
         Fraction f = new Fraction();
         System.out.println(f.toString());
+
+        //test avec seulement le numerateur
+        Fraction fN= new Fraction(10);
+        System.out.println(fN.toString());
+
+        //test avec num et deno
+        Fraction f2= new Fraction(12,2);
+        System.out.println(f2.toString());
+
+        System.out.println(f.getZERO());
+        System.out.println(f.getUN());
 
     }
     
